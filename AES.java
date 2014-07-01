de15525aa0a826cd2ca56fabf52ad44e25ce3bf0
fredaander <freda.ander@gmail.com>
2014-07-01 10:17:00 -0500

    added mixColumns and started addRoundKey

--- AES.java
+++ AES.java
@@ -3,8 +3,9 @@ import java.io.*;
 
 public class AES {
 
-	public static int[][] cryptMatrix = new int [4][4]; 
-	
+	public static int[][] cryptMatrix = new int[4][4]; 
+	public static int[][] roundKey;
+
 	public static int [][] sBox = new int [] []{ 
 		{0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76},
 		{0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0},
@@ -23,19 +24,87 @@ public class AES {
 		{0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf},
 		{0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16}
 	};
-	
-	
+
+	final static int[] LogTable = {
+		0,   0,  25,   1,  50,   2,  26, 198,  75, 199,  27, 104,  51, 238, 223,   3, 
+		100,   4, 224,  14,  52, 141, 129, 239,  76, 113,   8, 200, 248, 105,  28, 193, 
+		125, 194,  29, 181, 249, 185,  39, 106,  77, 228, 166, 114, 154, 201,   9, 120, 
+		101,  47, 138,   5,  33,  15, 225,  36,  18, 240, 130,  69,  53, 147, 218, 142, 
+		150, 143, 219, 189,  54, 208, 206, 148,  19,  92, 210, 241,  64,  70, 131,  56, 
+		102, 221, 253,  48, 191,   6, 139,  98, 179,  37, 226, 152,  34, 136, 145,  16, 
+		126, 110,  72, 195, 163, 182,  30,  66,  58, 107,  40,  84, 250, 133,  61, 186, 
+		43, 121,  10,  21, 155, 159,  94, 202,  78, 212, 172, 229, 243, 115, 167,  87, 
+		175,  88, 168,  80, 244, 234, 214, 116,  79, 174, 233, 213, 231, 230, 173, 232, 
+		44, 215, 117, 122, 235,  22,  11, 245,  89, 203,  95, 176, 156, 169,  81, 160, 
+		127,  12, 246, 111,  23, 196,  73, 236, 216,  67,  31,  45, 164, 118, 123, 183, 
+		204, 187,  62,  90, 251,  96, 177, 134,  59,  82, 161, 108, 170,  85,  41, 157, 
+		151, 178, 135, 144,  97, 190, 220, 252, 188, 149, 207, 205,  55,  63,  91, 209, 
+		83,  57, 132,  60,  65, 162, 109,  71,  20,  42, 158,  93,  86, 242, 211, 171, 
+		68,  17, 146, 217,  35,  32,  46, 137, 180, 124, 184,  38, 119, 153, 227, 165, 
+		103,  74, 237, 222, 197,  49, 254,  24,  13,  99, 140, 128, 192, 247, 112,   7};
+
+	final static int[] AlogTable = {
+		1,   3,   5,  15,  17,  51,  85, 255,  26,  46, 114, 150, 161, 248,  19,  53, 
+		95, 225,  56,  72, 216, 115, 149, 164, 247,   2,   6,  10,  30,  34, 102, 170, 
+		229,  52,  92, 228,  55,  89, 235,  38, 106, 190, 217, 112, 144, 171, 230,  49, 
+		83, 245,   4,  12,  20,  60,  68, 204,  79, 209, 104, 184, 211, 110, 178, 205, 
+		76, 212, 103, 169, 224,  59,  77, 215,  98, 166, 241,   8,  24,  40, 120, 136, 
+		131, 158, 185, 208, 107, 189, 220, 127, 129, 152, 179, 206,  73, 219, 118, 154, 
+		181, 196,  87, 249,  16,  48,  80, 240,  11,  29,  39, 105, 187, 214,  97, 163, 
+		254,  25,  43, 125, 135, 146, 173, 236,  47, 113, 147, 174, 233,  32,  96, 160, 
+		251,  22,  58,  78, 210, 109, 183, 194,  93, 231,  50,  86, 250,  21,  63,  65, 
+		195,  94, 226,  61,  71, 201,  64, 192,  91, 237,  44, 116, 156, 191, 218, 117, 
+		159, 186, 213, 100, 172, 239,  42, 126, 130, 157, 188, 223, 122, 142, 137, 128, 
+		155, 182, 193,  88, 232,  35, 101, 175, 234,  37, 111, 177, 200,  67, 197,  84, 
+		252,  31,  33,  99, 165, 244,   7,   9,  27,  45, 119, 153, 176, 203,  70, 202, 
+		69, 207,  74, 222, 121, 139, 134, 145, 168, 227,  62,  66, 198,  81, 243,  14, 
+		18,  54,  90, 238,  41, 123, 141, 140, 143, 138, 133, 148, 167, 242,  13,  23, 
+		57,  75, 221, 124, 132, 151, 162, 253,  28,  36, 108, 180, 199,  82, 246,   1};
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
+		cryptMatrix = setUpArray(args[2]); 
+		roundKey = setUpArray (args[1]); 
 		//java AES option keyFile inputFile
+			printMatrix();
+			System.out.println(); 
+
+			int i = 0; 
+			while (i < 9) {
+				runRounds();
+				i++; 
+			}
+	}
+	
+	private static void runRounds() {
+		subBytes();
+		printMatrix();
+		System.out.println(); 
+
+		shiftRows();
+		printMatrix();
+		System.out.println();
+
+		mixColumns(); 
+		printMatrix(); 
+		System.out.println(); 
+
+		addRoundKey(); 
+		printMatrix(); 
+		System.out.println(); 
+	}
+	
+	private static int[][] setUpArray(String file) {
 		BufferedReader in;
+		int [][] making = new int[4][4]; 
 		try {
-			in = new BufferedReader(new FileReader(args[2]));
+			in = new BufferedReader(new FileReader(file));
 			String line = in.readLine();
 			while (line !=null)
 			{	
-				
+
 				int col = 0;
 				int row = 0;
 				for (int i = 0; line != null && i<line.length(); i=i +2)
@@ -46,19 +115,12 @@ public class AES {
 						row++;
 					}
 					int hex = Integer.decode("0x"+line.substring(i,i+2));
-					
-					cryptMatrix[row][col] = hex;
+
+					making[row][col] = hex;
 					col++;
 				}
 				line = in.readLine();
-				
 			}
-			printMatrix();
-			subBytes();
-			printMatrix();
-			shiftRows();
-			printMatrix();
-			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -67,29 +129,7 @@ public class AES {
 			e.printStackTrace();
 		}
 		
-		
-		//Find out which option is given
-		String option = args[0];
-		if (option.equals("e"))
-		{
-			
-		}
-		else if (option.equals("d"))
-		{
-			
-		}
-		else
-		{
-			System.out.println("Invalid input.");
-			System.exit(1);
-		}
-		
-		//Read in keyFile
-		//Convert to Binary
-		//Encrypt
-		//Store as hex string to output file
-		
-		
+		return making; 
 	}
 
 	//print matrix
@@ -99,7 +139,7 @@ public class AES {
 		{
 			for (int j =0; j<cryptMatrix[i].length; j++)
 			{
-				System.out.print(cryptMatrix[i][j] + " ");
+				System.out.print(Integer.toHexString(cryptMatrix[i][j])+ " ");
 			}
 			System.out.println();
 		}
@@ -120,22 +160,22 @@ public class AES {
 			}
 			firstdigit = (cryptMatrix[row][col]>>4)&0xF;
 			lastdigit = cryptMatrix[row][col]&0xF;
-			//System.out.println(firstdigit +"\t"+ lastdigit+ "\t\t"+ sBox[firstdigit][lastdigit]);
+
 			cryptMatrix[row][col] = sBox[firstdigit][lastdigit];
 			col++;
 			parameter++;
 		}
 	}
-	
+
 	//shiftRows
 	public static void shiftRows(){
-		
+
 		//temporary variables with number indicating original column
 		int temp1=0;
 		int temp2=0;
 		int temp3=0;
 		int temp4=0;
-		
+
 		//first row stays the same
 		//second row: 2nd to 1st, 3rd to 2nd, 4th to 3rd, 1st goes to 4th
 		temp1=cryptMatrix[1][0];
@@ -146,8 +186,8 @@ public class AES {
 		cryptMatrix[1][1]= temp3;
 		cryptMatrix[1][2]= temp4;
 		cryptMatrix[1][3]= temp1;
-		
-		
+
+
 		//third row: 3rd to 1st, 4th to 2nd, 1st to 3rd, 2nd to 4th
 		temp1=cryptMatrix[2][0];
 		temp2=cryptMatrix[2][1];
@@ -157,8 +197,8 @@ public class AES {
 		cryptMatrix[2][1]= temp4;
 		cryptMatrix[2][2]= temp1;
 		cryptMatrix[2][3]= temp2;
-		
-		
+
+
 		//fourth row: 4th to 1st, 1st to 2nd, 2nd to 3rd, 3rd to 4th
 		temp1=cryptMatrix[3][0];
 		temp2=cryptMatrix[3][1];
@@ -168,11 +208,70 @@ public class AES {
 		cryptMatrix[3][1]= temp1;
 		cryptMatrix[3][2]= temp2;
 		cryptMatrix[3][3]= temp3;
-		
+
 	}
-	
+
 	//mixColumns
-	
+	public static void mixColumns() {
+		
+		for (int i = 0; i < cryptMatrix.length; i++){
+			mixColumn2(i); 
+		}
+	}
+
+	public static void mixColumn2 (int c) {
+		// This is another alternate version of mixColumn, using the 
+		// logtables to do the computation.
+
+		int a[] = new int[4];
+
+		// note that a is just a copy of st[.][c]
+		for (int i = 0; i < 4; i++) 
+			a[i] = cryptMatrix[i][c];
+
+		// This is exactly the same as mixColumns1, if 
+		// the mul columns somehow match the b columns there.
+		cryptMatrix[0][c] = (mul(2,a[0]) ^ a[2] ^ a[3] ^ mul(3,a[1]));
+		cryptMatrix[1][c] = (mul(2,a[1]) ^ a[3] ^ a[0] ^ mul(3,a[2]));
+		cryptMatrix[2][c] = (mul(2,a[2]) ^ a[0] ^ a[1] ^ mul(3,a[3]));
+		cryptMatrix[3][c] = (mul(2,a[3]) ^ a[1] ^ a[2] ^ mul(3,a[0]));
+	} // mixColumn2
+
+	public void invMixColumn2 (int c) {
+		int a[] = new int[4];
+
+		// note that a is just a copy of st[.][c]
+		for (int i = 0; i < 4; i++) 
+			a[i] = cryptMatrix[i][c];
+
+		cryptMatrix[0][c] = (mul(0xE,a[0]) ^ mul(0xB,a[1]) ^ mul(0xD, a[2]) ^ mul(0x9,a[3]));
+		cryptMatrix[1][c] = (mul(0xE,a[1]) ^ mul(0xB,a[2]) ^ mul(0xD, a[3]) ^ mul(0x9,a[0]));
+		cryptMatrix[2][c] = (mul(0xE,a[2]) ^ mul(0xB,a[3]) ^ mul(0xD, a[0]) ^ mul(0x9,a[1]));
+		cryptMatrix[3][c] = (mul(0xE,a[3]) ^ mul(0xB,a[0]) ^ mul(0xD, a[1]) ^ mul(0x9,a[2]));
+	} // invMixColumn2
+
+	private static int mul (int a, int b) {
+		byte b2 = (byte) (b & 0xFF); 
+		int inda = (a < 0) ? (a + 256) : a;
+		int indb = (b2 < 0) ? (b2 + 256) : b2;
+
+		if ( (a != 0) && (b != 0) ) {
+			int index = (LogTable[inda] + LogTable[indb]);
+			int val = (AlogTable[ index % 255 ] );
+			return val;
+		}
+		else 
+			return 0;
+	} // mul
+
 	//addRoundKey
-	
+	public static void addRoundKey(){
+
+		for (int i = 0; i < cryptMatrix[0].length; i++){
+			for (int j = 0; j < cryptMatrix.length; j++){
+				cryptMatrix[j][i] = cryptMatrix[j][i] ^ roundKey[j][i]; 
+			} 
+		}
+
+	}
 }
