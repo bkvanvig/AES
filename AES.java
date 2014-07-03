9dcc3afc730d94cdd9d67ef38bd31dbab5cb7c02
Brittany Kvanvig <brittany.kvanvig@utexas.edu>
2014-07-03 18:47:34 -0500

    final AES version

--- AES.java
+++ AES.java
@@ -29,23 +29,23 @@ public class AES {
 		{0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16}
 	};
 	public static int [][] inverseSBox = new int [] [] {
-	    {0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb},
-	    {0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb},
-	    {0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e},
-	    {0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25},
-	    {0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92},
-	    {0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84},
-	    {0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06},
-	    {0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b},
-	    {0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73},
-	    {0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e},
-	    {0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b},
-	    {0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4},
-	    {0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f},
-	    {0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef},
-	    {0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61},
-	    {0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d}
-	    };
+		{0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb},
+		{0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb},
+		{0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e},
+		{0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25},
+		{0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92},
+		{0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84},
+		{0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06},
+		{0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b},
+		{0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73},
+		{0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e},
+		{0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b},
+		{0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4},
+		{0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f},
+		{0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef},
+		{0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61},
+		{0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d}
+	};
 	final static int[] LogTable = {
 		0,   0,  25,   1,  50,   2,  26, 198,  75, 199,  27, 104,  51, 238, 223,   3, 
 		100,   4, 224,  14,  52, 141, 129, 239,  76, 113,   8, 200, 248, 105,  28, 193, 
@@ -147,41 +147,59 @@ public class AES {
 		{0xe8, 00, 00, 00}, {0xcb, 00, 00, 00}, {0x8d, 00, 00, 00}};
 
 	public static void main(String[] args) {
-		filename = args[2].toString();
-		cryptMatrix = setUpArray(args[2], 4); 
-		int[][] cipher = setUpArray (args[1], 8);
-		roundKey = setUpArray (args[1], 15*4);
+		
+		long starttime = System.nanoTime();
+		filename = args[2];
+		File key = new File (args[1]);
+		roundKey = setUpArray (key, 15*4);
+		int[][] cipher = setUpArray (key, 8);
 		decrypt = args[0].equals("d"); 
+		filename = (decrypt) ? filename + ".dec" : filename + ".enc";
 		
 		
-		//java AES option keyFile inputFile
-				System.out.println("Original Matrix Given:"); 
-				printMatrix(cryptMatrix);
-				System.out.println("Original CipherKey: ");
-				printMatrix(cipher);  
+		BufferedReader in;
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(filename));
+			in = new BufferedReader(new FileReader(args[2]));
+			String line = in.readLine();
+			
+			//java AES option keyFile inputFile
+			System.out.println("Original Matrix Given:"); 
+			System.out.println(printMatrix(cryptMatrix));
+			System.out.println("Original CipherKey: ");
+			System.out.println(printMatrix(cipher));  
 
-				int i = 0; 
-				keyIdx = 8;
+			int i = 0; 
+			keyIdx = 8;
 
-				while (keyIdx < roundKey[0].length){
-					updateRoundKey();
-				}
+			while (keyIdx < roundKey[0].length){
+				updateRoundKey();
+			}
 
-				System.out.println("RoundKey Expanded: "); 
-				for (i = 0; i<roundKey.length; i++)
+			System.out.println("RoundKey Expanded: "); 
+			for (i = 0; i<roundKey.length; i++)
+			{
+				for (int j =0; j<roundKey[i].length; j++)
 				{
-					for (int j =0; j<roundKey[i].length; j++)
-					{
-						String aHex = Integer.toHexString(roundKey[i][j]); 
-						if (aHex.length() == 1) aHex = "0"+ aHex; 
-						System.out.print(aHex + " ");
+					String aHex = Integer.toHexString(roundKey[i][j]); 
+					if (aHex.length() == 1) aHex = "0"+ aHex; 
+					System.out.print(aHex + " ");
 
 
-						if (((j+1)%4) == 0) System.out.print(" "); 
-					}
-					System.out.println();
+					if (((j+1)%4) == 0) System.out.print(" "); 
 				}
 				System.out.println();
+			}
+			System.out.println();
+			
+			while (line != null)
+			{	
+				int valid = setUpArray(line, 4); 
+				if (valid == -1)
+				{
+					line = in.readLine();
+					continue;
+				}
 
 
 				if (!decrypt){
@@ -191,6 +209,21 @@ public class AES {
 					sBox = inverseSBox; 
 					runD(); 
 				}
+				rconIdx = 0;
+				line = in.readLine();
+				out.write(printMatrix(cryptMatrix));
+			}
+			out.flush();
+			out.close();
+			in.close(); 
+			System.out.println((System.nanoTime()-starttime)/1000000.0);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
@@ -200,16 +233,16 @@ public class AES {
 
 		System.out.println("After addRoundKey(" + 14+ ")"); 
 		addRoundKey(); 
-		printMatrix(cryptMatrix); 
-		
+		System.out.println(printMatrix(cryptMatrix)); 
+
 		System.out.println("InverseShiftRows Results: " );
 		inverseShiftRows();
-		printMatrix(cryptMatrix); 
-		
+		System.out.println(printMatrix(cryptMatrix)); 
+
 		System.out.println("InverseSubBytes Results: " );
 		subBytes();
-		printMatrix(cryptMatrix);
-		
+		System.out.println(printMatrix(cryptMatrix));
+
 		while (i >0) {
 			runRoundsD(i);
 			i--; 
@@ -217,8 +250,8 @@ public class AES {
 
 		System.out.println("After addRoundKey(" + 0+ ")"); 
 		addRoundKey(); 
-		printMatrix(cryptMatrix); 
-		
+		System.out.println(printMatrix(cryptMatrix)); 
+
 		System.out.println("The Cipher Text: "); 
 		for (i = 0; i<cryptMatrix.length; i++)
 		{
@@ -234,22 +267,22 @@ public class AES {
 	}
 
 	private static void runRoundsD(int i) {
-		
+
 		System.out.println("After addRoundKey(" + i+ ")");
 		addRoundKey(); 
-		printMatrix(cryptMatrix); 
-		
+		System.out.println(printMatrix(cryptMatrix)); 
+
 		System.out.println("InverseMixColumns Results: " );
 		mixColumns(); 
-		printMatrix(cryptMatrix); 
-		
+		System.out.println(printMatrix(cryptMatrix)); 
+
 		System.out.println("InverseShiftRows Results: " );
 		inverseShiftRows();
-		printMatrix(cryptMatrix);
-		
+		System.out.println(printMatrix(cryptMatrix));
+
 		System.out.println("InverseSubBytes Results: " );
 		subBytes();
-		printMatrix(cryptMatrix); 
+		System.out.println(printMatrix(cryptMatrix)); 
 	}
 
 	private static void run(){
@@ -258,7 +291,7 @@ public class AES {
 
 		System.out.println("After addRoundKey(" + 0+ ")"); 
 		addRoundKey(); 
-		printMatrix(cryptMatrix); 
+		System.out.println(printMatrix(cryptMatrix)); 
 
 		while (i < 14) {
 			runRounds(i);
@@ -268,15 +301,15 @@ public class AES {
 
 		System.out.println("SubBytes Results: " );
 		subBytes();
-		printMatrix(cryptMatrix);
+		System.out.println(printMatrix(cryptMatrix));
 
 		System.out.println("ShiftRows Results: " );
 		shiftRows();
-		printMatrix(cryptMatrix); 
+		System.out.println(printMatrix(cryptMatrix)); 
 
 		System.out.println("After addRoundKey(" + (i)+ ")");
 		addRoundKey(); 
-		printMatrix(cryptMatrix); 
+		System.out.println(printMatrix(cryptMatrix)); 
 
 		System.out.println("The Cipher Text: "); 
 		for (i = 0; i<cryptMatrix.length; i++)
@@ -296,22 +329,22 @@ public class AES {
 	private static void runRounds(int i) {
 		System.out.println("SubBytes Results: " );
 		subBytes();
-		printMatrix(cryptMatrix);
+		System.out.println(printMatrix(cryptMatrix));
 
 		System.out.println("ShiftRows Results: " );
 		shiftRows();
-		printMatrix(cryptMatrix);
+		System.out.println(printMatrix(cryptMatrix));
 
 		System.out.println("MixColumns Results: " );
 		mixColumns(); 
-		printMatrix(cryptMatrix);  
+		System.out.println(printMatrix(cryptMatrix));  
 
 		System.out.println("After addRoundKey(" + i+ ")");
 		addRoundKey(); 
-		printMatrix(cryptMatrix); 
+		System.out.println(printMatrix(cryptMatrix)); 
 	}
 
-	private static int[][] setUpArray(String file, int size) {
+	private static int[][] setUpArray(File file, int size) {
 		BufferedReader in;
 		int [][] making = new int[4][size]; 
 		try {
@@ -322,14 +355,7 @@ public class AES {
 
 				int col = 0;
 				int row = 0;
-				
-				if (line.length() > 33 && size ==4)
-				{
-					System.out.println("This line has too many characters. Moving on.");
-					line = in.readLine();
-					continue;
-				}
-				
+
 				for (int i = 0; line != null && i<line.length()-1; i=i +2)
 				{
 					if (row == 4)
@@ -343,10 +369,8 @@ public class AES {
 						making[row][col] = hex;
 					} catch (NumberFormatException e) {
 						System.out.println("MISFORMATTED TEXT ERROR ON "+ line.substring(i,i+2));
-						System.exit(1);
 					}
 
-					
 					row++; 
 				}
 				line = in.readLine();
@@ -363,20 +387,47 @@ public class AES {
 		return making; 
 	}
 
+	private static int setUpArray(String line, int size) {
+		int [][] making = new int[4][size]; 
+		int col = 0;
+		int row = 0;
+
+		for (int i = 0; i<line.length()-1 && i <32; i=i +2)
+		{
+			if (row == 4)
+			{
+				row = 0;
+				col++;
+			}
+			int hex;
+			try {
+				hex = Integer.decode("0x"+line.substring(i,i+2));
+				making[row][col] = hex;
+			} catch (NumberFormatException e) {
+				System.out.println("MISFORMATTED TEXT ERROR ON "+ line.substring(i,i+2));
+				return -1;
+			}
+			row++; 
+		}
+
+		cryptMatrix = making; 
+		return 0;
+	}
+
 	//print matrix
 	//used for debugging purposes
-	public static void printMatrix (int[][] matrix) {
-
+	public static String printMatrix (int[][] matrix) {
+		String hexreturn = "";
 		for (int i = 0; i<matrix[0].length; i++)
 		{
 			for (int j =0; j<matrix.length; j++)
 			{
 				String aHex = Integer.toHexString(matrix[j][i]); 
 				if (aHex.length() == 1) aHex = "0"+ aHex; 
-				System.out.print(aHex);
+				hexreturn = hexreturn + aHex;
 			}
 		}
-		System.out.println("\n"); 
+		return hexreturn+"\n";
 
 	}
 	//subBytes
@@ -489,18 +540,18 @@ public class AES {
 	}
 
 	//mixColumns
-		public static void mixColumns() {
+	public static void mixColumns() {
 
-			if (decrypt){
-				for (int i = 0; i < cryptMatrix.length; i++){
-					invMixColumn2(i); 
-				}
+		if (decrypt){
+			for (int i = 0; i < cryptMatrix.length; i++){
+				invMixColumn2(i); 
 			}
-			else{
-				for (int i = 0; i < cryptMatrix.length; i++){
-					mixColumn2(i); 
-				}}
 		}
+		else{
+			for (int i = 0; i < cryptMatrix.length; i++){
+				mixColumn2(i); 
+			}}
+	}
 
 	public static void mixColumn2 (int c) {
 		// This is another alternate version of mixColumn, using the 
@@ -548,17 +599,17 @@ public class AES {
 	} // mul
 
 	//addRoundKey
-		public static void addRoundKey(){
+	public static void addRoundKey(){
 
-			for (int i = 0; i < cryptMatrix[0].length; i++){
-				for (int j = 0; j < cryptMatrix.length; j++){
-					cryptMatrix[j][i] = cryptMatrix[j][i] ^ roundKey[j][i+keyIdx]; 
-				} 
-			}
-
-			if (decrypt) keyIdx -= 4; 
-			else keyIdx +=4; 
+		for (int i = 0; i < cryptMatrix[0].length; i++){
+			for (int j = 0; j < cryptMatrix.length; j++){
+				cryptMatrix[j][i] = cryptMatrix[j][i] ^ roundKey[j][i+keyIdx]; 
+			} 
 		}
+
+		if (decrypt) keyIdx -= 4; 
+		else keyIdx +=4; 
+	}
 
 	//updates the RoundKey
 	public static void updateRoundKey(){
@@ -579,7 +630,7 @@ public class AES {
 		//then XOR next column with result
 		int[] rconWord = rcon[rconIdx]; 
 		int end = (keyIdx+8 > roundKey[0].length) ? keyIdx+4 : keyIdx+8; 
-		
+
 		for (int i = keyIdx; i < end; i++){
 			for (int j = 0; j < roundKey.length; j++){
 				if (i == keyIdx+4 && j ==0){rotWord = subBytesWord(rotWord);}
